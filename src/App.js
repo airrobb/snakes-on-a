94f3e5cd0e4b5f9aca2c9b507cbd10519447312e
Aaron Robb <airrobb@gmail.com>
2019-07-23 21:07:18 -0400

    Working hiss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+import CursorImage from "./assets/images/cursor-snake2.png";
+import Hiss from "./assets/audio/hissing.mp3";
+
+import contentfulClient from "./contentful";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    cursor: url(${CursorImage}), auto;
+
+  }
+
+  html, body { 
+    margin: 0;
+    padding: 0;
+  } 
+`;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  align-content: center;
+  justify-content: center;
+  grid-gap: 3rem;
+`;
+
+const Image = styled.div`
+  width: 40vw;
+  height: 40vw;
+  background-image: url(${({ src }) => src});
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  display: inline-block;
+  border: 2px solid black;
+
+  @media (max-width: 767px) {
+    width: 80vw;
+    height: 70vw;
+  }
+`;
+
+const Row = styled.div`
+  display: grid;
+  align-content: center;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  width: 15rem;
+  text-transform: uppercase;
+  font-size: 1.5rem;
+  line-height: 3rem;
+  border: 2px solid black;
+
+  &:active,
+  &:focus {
+    outline: none;
+  }
+`;
+
+const generateRandom = (max, current) => {
+  const random = Math.floor(Math.random() * Math.floor(max));
+  return current !== random ? random : generateRandom(current, max);
+};
 
 function App() {
+  const [snakes, setSnakes] = useState([]);
+  const [activeSnake, setActive] = useState();
+  const audioRef = useRef();
+
+  const getNewSnake = useCallback(() => {
+    const max = Object.keys(snakes).length;
+    audioRef.current.play();
+    if (max > 1) {
+      setActive(generateRandom(max, activeSnake));
+    }
+  }, [activeSnake, snakes, audioRef]);
+
+  useEffect(() => {
+    contentfulClient
+      .getEntries()
+      .then(snakes => {
+        const parsed = snakes.items.map(({ fields }) => {
+          const { image, ...snakeMeta } = fields;
+          const {
+            fields: { file }
+          } = image;
+
+          return {
+            ...snakeMeta,
+            url: `https:${file.url}`
+          };
+        });
+
+        const snakeMap = {};
+
+        parsed.forEach((snake, i) => {
+          snakeMap[i] = snake;
+        });
+        setSnakes(snakeMap);
+        setActive(generateRandom(parsed.length));
+      })
+      .catch(console.error);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <GlobalStyle />
+      <Wrapper>
+        {snakes[activeSnake] && (
+          <React.Fragment>
+            <Row>
+              <Image
+                src={snakes[activeSnake].url}
+                alt={snakes[activeSnake].flavorText}
+                title={snakes[activeSnake].flavorText}
+              />
+            </Row>
+            <Row>
+              <Button onClick={getNewSnake}>Snakes</Button>
+            </Row>
+          </React.Fragment>
+        )}
+      </Wrapper>
+      <audio src={Hiss} ref={audioRef} />
+    </React.Fragment>
   );
 }
 
---
+++ src/contentful.js
@@ -0,0 +1,8 @@
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: "hj9b3re47h5j",
+  accessToken: "ro85lvYPd_xkyDTMAresqs52AiVLOmdqImQ9kGYfdF8"
+});
+
+export default client;
